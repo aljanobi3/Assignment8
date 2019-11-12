5143e06ded1bbb066f79cf5fce3b36e3d44307df
MOHAMMAD ALJANOBI <54585390+aljanobi3@users.noreply.github.com>
2019-11-12 10:12:15 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,26 @@
 for(let i = 0; i < 1024; i++) {
-    document.querySelector(".app").innerHTML += '<div></div>';
-
+    document.querySelector(".app").innerHTML += '<div></div>'; 
 }
 
-document.body.addEventListener('click', event => {
-    event.target.style.backgroundColor = '#fff';
-    event.target.style.border = '1px solid #000';
-
-});
-document.addEventListener('keydown', event => {
-    if(event.key === 'z'){
-        document.querySelector(".app").style.backgroundColor = '#0f0';
-    }
-    if(event.key === 'x'){
-                document.querySelector(".app").style.backgroundColor = '#ff0';
-
-    }
-    if(event.key === 'c'){
-        document.querySelector(".app").style.backgroundColor = '#f00'; 
-});
-
-document.querySelector('.clear').addEventListener('click', event =>{
-    window.location = window.location;
-
-});
-
-document.body.addEventListener('click', event => {
-    if(event.zKey){
-        event.target.innerText = document.querySelector('#text').Value;
-        let red = document.querySelector('.red').value;
-        event.target.style.backgroundColor='rgb(' + red + ', 0, 0)';
-
-    }
-    if(event.zKey){
+document.body.addEventListener('mouseover', event => {
+    if(event.ctrlKey) {
+        event.target.innerText = document.querySelector('#text-input').value; 
+        let ironMaiden = document.querySelector('.red').value; 
+        event.target.style.backgroundColor = 'rgb(' + ironMaiden + ', 0, 0)'; 
+    } else if (event.altKey) {
         let green = document.querySelector('.green').value;
         event.target.style.backgroundColor='rgb( 0,' + green + ', 0)';
 
-    }
-
-    if(event.cKey){
+        
+    } else if(event.shiftKey) {
         let blue = document.querySelector('.blue').value;
         event.target.style.backgroundColor='rgb( 0, 0,' + blue + ')';
-
     }
+}); 
 
-});
+document.querySelector('.clear').addEventListener('click', event => {
+    document.querySelectorAll('.app div').forEach(element => {
+        element.style = ''; 
+        element.className = ''; 
+    }); 
+})
